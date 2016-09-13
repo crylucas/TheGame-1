7a762db80eaf60fdcc48fe31b31010048615f58e
huardm <huardm@meleze13.iut-infobio.priv.univ-lille1.fr>
2016-09-13 15:25:02 +0200

    Personnage done Inventaire done

--- src/thegame/utilitaire/Inventaire.java
+++ src/thegame/utilitaire/Inventaire.java
@@ -10,8 +10,42 @@ import java.util.List;
 
 /**
  *
- * @author huardm
+ * @author cryl & huardm
  */
 public class Inventaire {
     private List<Objet> listeObjet = new ArrayList();
+    
+    public void addInventaire(Objet objet) {
+        this.listeObjet.add(objet);
+    }
+    
+    public Objet chercheObjetInv(int prmId) {
+        Objet objReturn = null;
+        for(int i = 0; i < this.listeObjet.size(); i++) {
+            if(listeObjet.get(i).getId() == prmId) {
+                objReturn = listeObjet.get(i);
+            }
+        }
+        return objReturn;
+    }
+    
+    public void delInventaire(Objet prmObjet) {
+        int i = 0;
+        while (i < this.listeObjet.size()) {
+            if(this.listeObjet.get(i) == prmObjet) {
+                this.listeObjet.remove(i);
+                i = this.listeObjet.size();
+            }
+            i++;
+        }
+    }
+    public List<Objet> getListeObjet(){
+        return this.listeObjet;
+    }
+    public void tousLesObjet(){
+        for(int i = 0; i<this.listeObjet.size(); i++){
+            Objet obj = this.listeObjet.get(i); 
+            System.out.println(obj.getId()+" "+obj.getName()+" Attaque = "+obj.getAttaque()+" Défense = "+obj.getDefense()+" Niveau = "+obj.getNiveau());
+        }
+    }
 }
